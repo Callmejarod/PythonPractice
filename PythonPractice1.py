fff01482c8e16348504e74234ad4e51ea7ed3ba5
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2023-10-16 16:35:51 -0700

    Changes from print(bad) to print(Error)

---
+++ PythonPractice1.py
@@ -0,0 +1,11 @@
+#For the users to input their answers
+
+first = input('First: ')
+second = input('Second: ')
+#Instead of using int, use float for decimal point numbers
+
+sum = float(first) + float(second)
+#Use the str to convert and print the input back into a string varible
+
+print('Sum:' + str(sum))
+#This is a comment
---
+++ PythonPractice2.py
@@ -0,0 +1,6 @@
+
+course = 'Python for Beginners'
+#The replace function replaces the string of for' into '4'
+#course.replace is a method)
+#course.blank can be any method
+print(course.replace('for', '4'))
